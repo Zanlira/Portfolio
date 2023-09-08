4b8f3681f6531ec393038ffd97d55d46efbd9767
SantiagoLira <zantlira@gmail.com>
2023-09-08 05:34:55 -0600

    add nav and footer

---
+++ src/views/InicioView.vue
@@ -0,0 +1,67 @@
+<template>
+    <h1>hola</h1>
+    <!-- <section class="container">
+        <div>
+            <h1>{{ Hola }}</h1>
+            <h3>{{ obj.nombre }}</h3>
+            <div class="pb-3">
+                <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip">
+                    Custom tooltip
+                </button>
+            </div>
+            <div>
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#Texto"
+                    aria-expanded="false" aria-controls="Texto">
+                    collapse
+                </button>
+            </div>
+            <div class="collapse" id="Texto">
+                <div class="card card-body">
+                    Frameworks:
+                    <ul class="list-group">
+                        <li class="list-group-item">NodeJs</li>
+                        <li class="list-group-item">Laravel</li>
+                        <li class="list-group-item">Angular</li>
+                        <li class="list-group-item">VueJs</li>
+                        <li class="list-group-item">React</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Dropdown button
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+        </div>
+    </section> -->
+</template>
+<script>
+// import { Tooltip } from "bootstrap";
+export default {
+    data() {
+        return {
+
+        };
+    },
+    mounted() {
+        // new Tooltip('[data-bs-toggle="tooltip"]', {
+        //     title: "Es un tooltip",
+        //     placement: "top",
+        //     customClass: "custom-tooltip",
+        // });
+    },
+};
+</script>
+
+<style>
+/* .custom-tooltip {
+    --bs-tooltip-bg: var(--bs-indigo);
+    --bs-tooltip-color: var(--bs-white);
+} */
+</style>
